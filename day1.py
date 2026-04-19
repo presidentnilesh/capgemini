008a42f48156dc45cfec05cdc6623b234c42e779
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-04-19 14:08:28 +0530

    day1 of capgemini

---
+++ day1.py
@@ -0,0 +1,9 @@
+print (int())       #default value for int 
+print(float())       # default value for float 
+a = 0 
+print(type(a))
+b = 10 + 4j 
+print(type(b))
+print(complex())       # # default value for complex
+
+
