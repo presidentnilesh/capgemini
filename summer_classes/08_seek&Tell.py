8fb4db9682378e9ba6960bdbe65f1b04d35c653a
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-06-18 15:48:51 +0530

    seek and tell

---
+++ summer_classes/08_seek&Tell.py
@@ -0,0 +1,29 @@
+# seek during write
+
+# f = open("s2.txt" , 'w')
+# f.write("hi\n hello \n byr\n kaiashe hia aap \n")
+# f.close()
+
+
+# f = open('s2.txt' , 'r')
+# print(f.read(2))
+# print(f.tell())
+# print(f.read(3))
+# print(f.tell())
+# print(f.seek(0))
+
+# print(f.read(2))
+# print(f.seek(8))
+# print(f.read())
+
+# take input in a list using file handling 
+# f= open('s3.txt' , 'w')
+# f.write(" [1,2,3,4,5,6,7,8,9,10]")
+# f.close()
+
+# list-comprehension
+
+a = [ i**2 for i in range(10) if i %2 == 0 ]    #jaishe sign hamlog denga waishe he type cast khud sha ho jayega 
+b = { m : m**2 for m in range (14) if m%3 ==0 }
+print(a)
+print(b)
