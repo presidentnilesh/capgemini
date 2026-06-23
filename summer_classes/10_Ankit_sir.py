78216a36c92f39a59918df1e34ca4d1cd0582239
Nilesh Kumar <nileshkumar12521@gmail.com>
2026-06-23 09:06:38 +0530

    iter

---
+++ summer_classes/10_Ankit_sir.py
@@ -0,0 +1,31 @@
+# Question 1
+# Create a list of fruits and display each fruit using iter() and next().
+fruits = [ 'mango' , 'papaya' , 'grapes' , 'apple']
+fruit_iter = iter(fruits)
+
+print(next(fruit_iter))
+print(next(fruit_iter))
+print(next(fruit_iter))
+print(next(fruit_iter))
+
+
+# Question 2
+# Create a list of numbers and display all numbers one by one using next().
+num = [ 1,2,3,4,5,6]
+iter_num = iter(num)
+
+print(next(iter_num))
+print(next(iter_num))
+print(next(iter_num))
+print(next(iter_num))
+
+
+# Question 3
+# Create a tuple of subject names and access each subject using an iterator.
+subject = ['english' ,'hindi' , 'maths' , 'science']
+iterr_sub = iter(subject)
+
+print(next(iterr_sub))
+print(next(iterr_sub))
+print(next(iterr_sub))
+print(next(iterr_sub))
